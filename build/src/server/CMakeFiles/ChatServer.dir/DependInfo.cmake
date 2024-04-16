23ca83a89e2b3275b2f523c234187e1903520123
ahua <1761569556@qq.com>
2024-04-16 16:51:01 +0800

    提交集群聊天服务器项目代码

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/chatserver.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/chatservice.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/db/db.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/main.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/model/friendmodel.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/model/groupmodel.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/model/offlinemessagemodel.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/model/usermodel.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/lihuimin/Project/chat/Clustered-ChatServer/src/server/redis/redis.cpp" "/home/lihuimin/Project/chat/Clustered-ChatServer/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
